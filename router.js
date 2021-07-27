a20ef5ab711e3d24decb5248c665eae9af40d19c
Mostapha Rammo <moose@Mostaphas-MacBook-Air.local>
2021-07-27 14:45:01 -0400

    better router

--- router.js
+++ router.js
@@ -1,27 +1,11 @@
-const passport = require('passport');
 const UserRouter = require('./api/users/user.router');
 const AuthRouter = require('./api/auth/auth.router');
 const RecipeRouter = require('./api/recipes/recipe.router');
 const ThemeRouter = require('./api/theme/theme.router');
 
-const routes = {
-  private: {
-  },
-  public: {
-    '/api/users': UserRouter,
-    '/api/recipes': RecipeRouter,
-    '/api/theme': ThemeRouter,
-    '/auth': AuthRouter,
-  }
-}
-
 exports.wire = function wire(app) {
-  for (const [key, value] of Object.entries(routes.public)) {
-    app.use(key, value);
-  }
-
-  app.use(passport.authenticate('jwt'));
-  for (const [key, value] of Object.entries(routes.private)) {
-    app.use(key, value);
-  }
+  app.use('/api/users', UserRouter);
+  app.use('/api/recipes', RecipeRouter);
+  app.use('/api/theme', ThemeRouter);
+  app.use('/auth', AuthRouter);
 }
